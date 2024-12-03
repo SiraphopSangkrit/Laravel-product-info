7edea2f1da33bebaa86d69ed789695f75e5d96c9
Siraphop Sangkrit <Siraphop_sangkrit@kkumail.com>
2024-12-03 16:40:14 +0700

    update add picture

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -149,16 +149,16 @@ class AdminController extends Controller
         $product_id = $request->product_id;
 
         $images = $request->file('image');
-        if ($images) {
-            foreach ($images as $image) {
-                $product_pic = new ProductPictures;
-                $product_pic->product_id = $product_id;
-                $picture_path = $image->store('products_image', 'public');
-                $product_pic->asset_url = $picture_path;
-                $product_pic->public_url = $picture_path;
-                $product_pic->save();
-            }
+
+        foreach ($images as $image) {
+            $product_pic = new ProductPictures;
+            $product_pic->product_id = $product_id;
+            $picture_path = $image->store('products_image', 'public');
+            $product_pic->asset_url = $picture_path;
+            $product_pic->public_url = $picture_path;
+            $product_pic->save();
         }
+
 
         return redirect()->back();
     }
@@ -468,16 +468,16 @@ class AdminController extends Controller
         $news_id = $request->news_id;
 
         $images = $request->file('image');
-        if ($images) {
-            foreach ($images as $image) {
-                $newsPic = new NewsPics;
-                $newsPic->news_id = $news_id;
-                $picture_path = $image->store('news_image', 'public');
-                $newsPic->asset_url = $picture_path;
-                $newsPic->public_url = $picture_path;
-                $newsPic->save();
-            }
+
+        foreach ($images as $image) {
+            $newsPic = new NewsPics;
+            $newsPic->news_id = $news_id;
+            $picture_path = $image->store('news_image', 'public');
+            $newsPic->asset_url = $picture_path;
+            $newsPic->public_url = $picture_path;
+            $newsPic->save();
         }
+
         return redirect()->back();
     }
 
